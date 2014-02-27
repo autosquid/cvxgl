f0a10a21fd6e86d40bcbfba514a557d78287a37c
uuplusu <justin.seeley.cn@gmail.com>
2014-02-27 15:02:20 +0800

    pass test

--- killer/cvxgl.hpp
+++ killer/cvxgl.hpp
@@ -1,10 +1,14 @@
 #ifndef _CHAOJIWUDI_SUPER_PAIN_KILLER_BY_UU
 #define _CHAOJIWUDI_SUPER_PAIN_KILLER_BY_UU
 #include <glm/glm.hpp>
+#include <utility>
+#include <math.h>
 #include <glm/gtc/matrix_transform.hpp>
 
 namespace cvxgl{
-    void cv2gl(
+    typedef std::pair<glm::ivec4, glm::vec4> GL_PARAM;
+
+    GL_PARAM cv2gl(
             glm::mat3 intrinsic,
             glm::ivec2 im_size
             ){
@@ -19,12 +23,21 @@ namespace cvxgl{
         float near_clip = 0.1;
         float far_clip = 200.0;
 
-        glm::vec4 viewport = glm::vec4(uc - width/2.0, vc - height/2.0, width, height);
-        glm::mat4 perspective = glm::perspective(
+        glm::ivec4 viewport = glm::ivec4(int(uc - width/2.0 + 0.5),
+                int(vc - height/2.0 + 0.5), int(width), int(height));
+        glm::vec4 perspective = glm::vec4(
                 float(2 * atan(height/2.f/(r * f /dy))/M_PI * 180),
-                float(width)/height* (dx/dy),
+                float(width)/height * (dx/dy),
                 near_clip,
                 far_clip);
+        return std::make_pair(viewport, perspective);
+    }
+
+
+    glm::mat4 perspective_matrix(float fov, float aspect, float znear, float zfar)
+    {
+        glm::mat4 perp = glm::perspective(fov, aspect, znear, zfar);
+        return perp;
     }
 
     glm::mat3 gl2cv(
@@ -32,19 +45,18 @@ namespace cvxgl{
             float fovy,
             float aspect,
             float r = 1
-            ){
+            )
+     {
         float uc = viewport[2] / 2.0 + viewport[0];
         float vc = viewport[3] / 2.0 + viewport[1];
-        float fy = 1.0f/tan(fovy / 2.0) * viewport[3] /2.0 / r;
-        float fx = fy / (aspect * viewport[3] / viewport[2]);
+        float fy = 1.0f/tan(fovy/ 180.0 * M_PI / 2.0) * viewport[3] /2.0 / r;
+        float fx = fy / aspect / viewport[3] * viewport[2];
 
         glm::mat3 intrinsic = glm::mat3(
                 fx, 0, uc,
-                0, fy, uc,
+                0, fy, vc,
                 0, 0, 1);
         return intrinsic;
     }
 };
 #endif
-
-
--- killer/main.cpp
+++ killer/main.cpp
@@ -3,15 +3,55 @@
 #include <boost/range/algorithm/copy.hpp>
 #include <boost/range.hpp>
 #include <iterator>
+#include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp>
 #include "cvxgl.hpp"
 
 
-int main(int argc, char ** argv)
+int hell()
 {
     std::string greeting("Hello World");
     boost::copy(boost::make_iterator_range(greeting.begin(), greeting.end()),
             std::ostream_iterator<std::string::value_type>(std::cout, ""));
+    return 0;
 }
 
 
+glm::mat3 test_intrinsic_data()
+{
+    float intrinsic[] =  { 1.8742872389462805e+003, 0., 9.5950000000000000e+002, 0.,
+       1.8742872389462805e+003, 5.3950000000000000e+002, 0., 0., 1. };
+    return glm::make_mat3(intrinsic);
+}
+
+
+int main(int argc, char ** argv){
+    auto intrinsic = test_intrinsic_data();
+    auto im_size = glm::ivec2(1920, 1080);
+    auto gl_pram = cvxgl::cv2gl(intrinsic, im_size);
+
+    std::cout<<"from cv: viewport:"<<std::endl;
+    boost::copy(boost::make_iterator_range(glm::value_ptr(gl_pram.first),
+                glm::value_ptr(gl_pram.first) + 4),
+            std::ostream_iterator<int>(std::cout, " "));
+    std::cout<<std::endl;
+
+    std::cout<<"from cv: perspective:"<<std::endl;
+    boost::copy(boost::make_iterator_range(glm::value_ptr(gl_pram.second),
+                glm::value_ptr(gl_pram.second) + 4),
+            std::ostream_iterator<float>(std::cout, " "));
+
+    std::cout<<std::endl;
+
+    auto intrinsic_r = cvxgl::gl2cv(
+            gl_pram.first,
+            gl_pram.second[0],
+            gl_pram.second[1]);
+
+    std::cout<<"recovered cv: intrinsic:"<<std::endl;
+    std::copy(glm::value_ptr(intrinsic_r), glm::value_ptr(intrinsic_r) + 9,
+            std::ostream_iterator<float>(std::cout," "));
+
+    std::cout<<std::endl;
+}
 
